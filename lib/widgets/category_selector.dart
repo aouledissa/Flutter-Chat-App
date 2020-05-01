344c13d309363e8dd550b908bbce06f8ccb92e91
aouledissa <devmed01@gmail.com>
2020-05-02 01:47:27 +0200

    Created CategorySelector widget

---
+++ lib/widgets/category_selector.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategorySelector extends StatelessWidget {
+  const CategorySelector({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90.0,
+      color: Colors.blue,
+    );
+  }
+}
