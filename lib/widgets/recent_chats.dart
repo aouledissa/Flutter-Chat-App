3ca249ded5e6b2380d322115f1c759ac9148ba99
aouledissa <devmed01@gmail.com>
2020-05-03 01:53:24 +0200

    refactored RecentChat widget

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -1,6 +1,9 @@
 import 'package:chat_app/models/message.dart';
 import 'package:flutter/material.dart';
 
+import 'chat_info.dart';
+import 'chat_tile.dart';
+
 class RecentChats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,11 +16,20 @@ class RecentChats extends StatelessWidget {
             topRight: Radius.circular(30.0),
           ),
         ),
-        child: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Text(chats[index].sender.name);
-          },
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Message chat = chats[index];
+              return ChatTile(
+                chat: chat,
+              );
+            },
+          ),
         ),
       ),
     );
